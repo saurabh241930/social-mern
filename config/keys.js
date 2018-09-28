5ac004ff41f66028595ed6153901790a5d64bb83
Saurabh <30368181+saurabh241930@users.noreply.github.com>
2018-09-28 15:57:05 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI:"mongodb://saurabh:100rabhh@ds115613.mlab.com:15613/social-mern-db"
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const port = process.env.PORT || 3000
+const users = require("./routes/api/users")
+const profile = require("./routes/api/profile")
+const posts = require("./routes/api/posts");
+
+const db = require("./config/keys").mongoURI;
+
+
+mongoose.connect(db)
+.then(() => console.log("Database connected"))
+.catch((err) => console.log(err))
+
+app.use("/api/users",users)
+app.use("/api/profile",profile)
+app.use("/api/posts",posts)
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
