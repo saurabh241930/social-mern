ff0f67709211f37b975008dca48cf65d038f6246
Saurabh <30368181+saurabh241930@users.noreply.github.com>
2018-09-28 16:01:16 +0530

    commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+
+//@route    GET api/users/test
+//@desc     test route
+//@access   PUBLIC
 router.get("/test",(req,res) => {
   res.json({msg:"it works"})
 })
